2031f1f8f227eb76f5dbb6cc9c090711288356a2
Antonis Liles <2578195L@student.gla.ac.uk>
2023-02-14 04:37:22 +0000

    first commit

--- tango_with_django_project/urls.py
+++ tango_with_django_project/urls.py
@@ -18,9 +18,13 @@ from django.urls import path
 from django.urls import include
 from rango import views
 
+from django.conf import settings
+from django.conf.urls.static import static
+
 
 urlpatterns = [
     path('', views.index, name='index'), #if its empty after the url go to index view
     path('rango/', include('rango.urls')), #if rango/ then check the project's urls
     path('admin/', admin.site.urls),
-]
+] + static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
+
