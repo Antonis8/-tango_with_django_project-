1374dd650030de38f0174a807bba4575ffab1102
Antonis Liles <2578195L@student.gla.ac.uk>
2023-02-18 04:19:12 +0000

    ch6 commit

--- rango/urls.py
+++ rango/urls.py
@@ -4,5 +4,7 @@ from rango import views
 app_name = 'rango'
 urlpatterns = [
     path('', views.index, name = 'index'), #after /rango
-    path('about/', views.about, name = 'about') 
+    path('about/', views.about, name = 'about'),
+    path('category/<slug:category_name_slug>/', views.show_category, name='show_category'),
+
 ]
--- rango/views.py
+++ rango/views.py
@@ -1,11 +1,42 @@
 from django.shortcuts import render
+from rango.models import Category, Page
 
 # Create your views here.
 
 from django.http import HttpResponse
+
+def show_category(request, category_name_slug):
+    context_dict = {}
+    try:
+        # Can we find a category name slug with the given name?
+        # If we can't, the .get() method raises a DoesNotExist exception.
+        category = Category.objects.get(slug=category_name_slug)
+        pages = Page.objects.filter(category=category)
+    
+        context_dict['pages'] = pages
+        context_dict['category'] = category
+
+    except Category.DoesNotExist:
+
+        # We get here if we didn't find the specified category.
+        # Don't do anything -
+        # the template will display the "no category" message for us.
+        context_dict['category'] = None
+        context_dict['pages'] = None
+
+    return render(request, 'rango/category.html', context=context_dict)
+
+
+
 def index(request):
 
-    context_dict = {'boldmessage': 'Crunchy, creamy, cookie, candy, cupcake!'} #boldmessage will become crunchy etc
+    category_list = Category.objects.order_by('-likes')[:5]
+    pages_list = Page.objects.order_by('-views')[:5]
+    context_dict = {}
+    context_dict['boldmessage']=  'Crunchy, creamy, cookie, candy, cupcake!' #boldmessage will become crunchy etc
+    context_dict["categories"] = category_list
+    context_dict['pages'] = pages_list
+    
     return render(request, 'rango/index.html', context= context_dict)
 
     
